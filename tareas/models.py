c507c85c858bcfec2a6361dbae8da963d9dedac8
alexayalacobel-spec <alex.ayala.cobel@gmail.com>
2026-04-29 08:42:35 -0600

    feat(app): Las tareas de login ya pueden ser creadas

--- tareas/models.py
+++ tareas/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Tarea(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField(blank=True)
     completada = models.BooleanField(default=False)
     creada = models.DateTimeField(auto_now_add=True)
-    prioridad = models.IntegerField(default=1)
 
     def __str__(self):
         return self.titulo
--- tareas/views.py
+++ tareas/views.py
@@ -1,29 +1,37 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tarea
 from .forms import TareaForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def lista_tareas(request):
-    tareas = Tarea.objects.order_by('-creada')
-    return render(request, 'lista.html', {'tareas': tareas})
+    tareas = Tarea.objects.filter(usuario=request.user)
+    return render(request, 'tareas/lista.html', {'tareas': tareas})
 
+@login_required
 def crear_tarea(request):
     if request.method == 'POST':
-        form = TareaForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('lista')
-    else:
-        form = TareaForm()
+        titulo = request.POST.get('titulo')
+        descripcion = request.POST.get('descripcion')
 
-    return render(request, 'tareas/crear.html', {'form': form})
+        Tarea.objects.create(
+            usuario=request.user,
+            titulo=titulo,
+            descripcion=descripcion
+        )
+        return redirect('lista')
 
+    return render(request, 'tareas/crear.html')
+
+@login_required
 def completar_tarea(request, id):
     tarea = Tarea.objects.get(id=id)
     tarea.completada = True
     tarea.save()
     return redirect('lista')
 
+@login_required
 def eliminar_tarea(request, id):
-    tarea = Tarea.objects.get(id=id)
+    tarea = get_object_or_404(Tarea, id=id, usuario=request.user)
     tarea.delete()
     return redirect('lista')
